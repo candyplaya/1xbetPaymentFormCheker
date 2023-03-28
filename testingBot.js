c9e55af44e9524534a5d7949a782a418bf9f27fb
candyplaya <121291639+candyplaya@users.noreply.github.com>
2023-03-28 11:48:34 +0300

    Update testingBot.js

--- testingBot.js
+++ testingBot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const cheking1xStart = require('./paymentFormCheker');
 
-const token = '2071463652:AAE1FYpYBn6kGrt26JAhzlbSw3DUNtAFu18';
+const token = '{{BOT_TOKEN_TG}}';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/1xbet/, async (msg) => {
