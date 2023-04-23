12f395478abf4edceba9fee709ee85ff02a65ae1
candyplaya <121291639+candyplaya@users.noreply.github.com>
2023-04-24 02:25:16 +0300

    Update paymentFormCheker.js

--- paymentFormCheker.js
+++ paymentFormCheker.js
@@ -1,41 +1,41 @@
 const puppeteer = require('puppeteer')
 
-async function cheking1xStart() {
+async function cheking1xStart(bettingUrl) {
     const browser = await puppeteer.launch(
         {
             headless: false,
             args: ['--no-sandbox'],
-            // executablePath: '/home/qa@kadmin/.config/google-chrome/default'
+            executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe'
         }
     )
     const page = await browser.newPage()
-    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36');
 
     await page.setViewport({
-        width: 1600,
-        height: 1300
+        width: 1200,
+        height: 700
     });
 
 
     let frameHandle
     let frame
     let payUrl
-    const mirrorUrl = 'https://lite-1x05933835.top'
+    const mirrorUrl = bettingUrl
     const methodChekingArray = []
-    // const methodArray = ['visa: ', 'mastercard: ', 'mir: ', 'maestro: ']
-    const methodArray = ['visa: ', 'maestro: ']
+    const methodArray = ['visa: ', 'mastercard: ', 'mir: ', 'maestro: ']
     const result = [];
 
+    try {
+
+    await loginVKacc()
+
     await page.goto(mirrorUrl)
-    // await new Promise(resolve => setTimeout(resolve, 70000));
     await new Promise(resolve => setTimeout(resolve, 7000));
-    // await getLoginWindow()
-    await inputLoginAcc()
+    await getLoginWindow()
     await takeUrlForMethod('visa')
-    // await page.goto(mirrorUrl)
-    // await takeUrlForMethod('mastercard')
-    // await page.goto(mirrorUrl)
-    // await takeUrlForMethod('mir')
+    await page.goto(mirrorUrl)
+    await takeUrlForMethod('mastercard')
+    await page.goto(mirrorUrl)
+    await takeUrlForMethod('mir')
     await page.goto(mirrorUrl)
     await takeUrlForMethod('maestro')
 
@@ -58,11 +58,14 @@ async function cheking1xStart() {
         const curPayUrl = page.url();
         payUrl = curPayUrl.split("/").slice(2, 3).toString();
         methodChekingArray.push(payUrl)
-        // await page.screenshot({ path: 'exemplse.png' })
         await new Promise(resolve => setTimeout(resolve, 7000));
     }
 
     async function iFrameCondition() {
+        if (await frame.$('#fullname')) {
+            await frame.type('#fullname', 'Ivan Lastov');
+        }
+
         if (await frame.$('#first_name')) {
             await frame.type('#first_name', 'Ivan');
         }
@@ -82,33 +85,14 @@ async function cheking1xStart() {
         await frame.click('#deposit_button')
     }
 
-    async function inputLoginAcc() {
-        findElementOnValueAndClick('.caption__label', 'Регистрация')
-        new Promise(resolve => setTimeout(resolve, 5000));
-
-        await page.waitForSelector('.registration-content__container');
-        await page.focus('.registration-content__container')
-
-        findElementOnValueAndClick('.caption__label', 'В 1 клик')
-        new Promise(resolve => setTimeout(resolve, 5000));
-
-        findElementOnValueAndClick('.caption__label', 'Зарегистрироваться')
-        new Promise(resolve => setTimeout(resolve, 5000));
-
-    }
-
     async function getLoginWindow() {
         findElementOnValueAndClick('.caption__label', 'Вход');
 
         await new Promise(resolve => setTimeout(resolve, 7000));
 
-        await page.click('#username');
-        await page.type('#username', '584434565');
+        await page.click('.ico--vk');
 
-        await page.click('#username-password');
-        await page.type('#username', 'zs6nzmt5');
-
-        findElementOnValueAndClick('.caption__label', 'Войти')
+        await new Promise(resolve => setTimeout(resolve, 5000));
     }
 
     async function goToPaymentMethodWindow() {
@@ -134,6 +118,23 @@ async function cheking1xStart() {
         }
     }
 
+    async function loginVKacc() {
+        await page.goto('https://vk.com/')
+        await page.waitForSelector('#index_email')
+        await page.click('#index_email')
+        await page.type('#index_email', '~EMAIL~')
+
+        findElementOnValueAndClick('.FlatButton__content', 'Войти')
+        await new Promise(resolve => setTimeout(resolve, 7000));
+        await page.type('[name="password"]', 'Test10Code')
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        await page.click('.vkuiButton__in')
+        await new Promise(resolve => setTimeout(resolve, 10000));
+    }
+} catch {
+    console.error('123');
+    await browser.close()
+}
 }
 
 module.exports = cheking1xStart
